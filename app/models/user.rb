502fc2619af50a29dc7f8aa6ee53bac509c0e75a
AyupovFN <kapterka@bk.ru>
2020-04-21 23:47:45 +0300

    edit usercontroller, model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,19 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   # Юзер может создавать много событий
   has_many :events
 
-  # У юреза должно быть имя не длиннее 35 букв
-  validates :name, presence: true, length: {maximum: 35}
+    validates :name, presence: true, length: {maximum: 35}
 
-  # У юзера должен быть уникальный email по заданному шаблону не длиннее 255
-  # символов
-  validates :email, presence: true, length: {maximum: 255}
-  validates :email, uniqueness: true
-  validates :email, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
+  # метод set_name
+  before_validation :set_name, on: :create
+
+  private
+
+  # Задаем юзеру случайное имя, если оно пустое
+  def set_name
+    self.name = "Товарисч №#{rand(777)}" if self.name.blank?
+  end
 end
